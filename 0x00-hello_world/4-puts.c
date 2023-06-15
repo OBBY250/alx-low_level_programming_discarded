1b58f2e27c0f76eb21c4208fc1f3203b8ce9bdd7
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-06-15 16:53:36 -0700

    my first script on c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is sane insane
+ * return: 0 (success)
+ */
+
+int main(viod)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
