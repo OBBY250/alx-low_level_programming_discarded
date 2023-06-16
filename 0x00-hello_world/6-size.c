eb0ab6f34304105a5b5d268630792efb1b8ec6be
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-06-15 18:17:27 -0700

    my first script on c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 	char d;
 	long int f;
 
-printf("The size of an int is %lu bytes\n", (unsigned long)sizeof(a));
-printf("The size of a float is %lu bytes\n", (unsigned long)sizeof(b));
-printf("The size of a long long int is %lu bytes\n", (unsigned long)sizeof(c));
-printf("The size of a char is %lu bytes\n", (unsigned long)sizeof(d));
-printf("The size of a long int is %lu bytes\n", (unsigned long)sizeof(f));
+printf("The size of an int is %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("The size of a float is %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("The size of a long long int is %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("The size of a char is %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("The size of a long int is %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return (0);
 }
