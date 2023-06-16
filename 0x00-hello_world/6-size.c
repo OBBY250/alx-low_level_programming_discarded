450a3307cf795308fa18bf9022a7588e96495c0d
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-06-15 17:48:23 -0700

    my first script on c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,13 @@
  * main - this program prints the size of various types of data
  * Return: 0 (success)
  */
-int main(void) 
+int main(void)
 {
-  printf("The size of an int is %zu bytes\n", sizeof(int));
-  printf("The size of a float is %zu bytes\n", sizeof(float));
-  printf("The size of a double is %zu bytes\n", sizeof(double));
-  printf("The size of a char is %zu bytes\n", sizeof(char));
-  printf("The size of a pointer is %zu bytes\n", sizeof(void*));
+	printf("The size of an int is %zu bytes\n", sizeof(int));
+	printf("The size of a float is %zu bytes\n", sizeof(float));
+	printf("The size of a long long int is %zu bytes\n", sizeof(long long int));
+	printf("The size of a char is %zu bytes\n", sizeof(char));
+	printf("The size of a long int is %zu bytes\n", sizeof(long int));
 
-  return 0;
+	return 0;
 }
-
