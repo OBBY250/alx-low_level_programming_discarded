aa7e0f0658f8a8bc062c082df012b8ba98953cc7
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-06-15 17:15:48 -0700

    my first script on c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - this will be used by gcc -wall option
  * Return: 0 (success)
  */
-int main(viod)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
