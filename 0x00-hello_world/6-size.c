e2593dda02d215dd9c4dd1efe3bf66849530a630
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-06-15 17:55:15 -0700

    my first script on c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	printf("The size of an int is %zu bytes\n", sizeof(int));
-	printf("The size of a float is %zu bytes\n", sizeof(float));
-	printf("The size of a long long int is %zu bytes\n", sizeof(long long int));
-	printf("The size of a char is %zu bytes\n", sizeof(char));
-	printf("The size of a long int is %zu bytes\n", sizeof(long int));
+	printf("The size of an int is %lu bytes\n", sizeof(int));
+	printf("The size of a float is %lu bytes\n", sizeof(float));
+	printf("The size of a long long int is %lu bytes\n", sizeof(long long int));
+	printf("The size of a char is %lu bytes\n", sizeof(char));
+	printf("The size of a long int is %lu bytes\n", sizeof(long int));
 
 	return (0);
 }
