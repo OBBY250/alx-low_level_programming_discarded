2c9ff355fc7e52eaecaf7fcab33ac1aa3be2943f
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-08-09 01:12:07 -0700

    revise

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program that uses "puts" to print
+ * return: 0 (success)
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
