6c322014a88b2319d1a5da629bdaffb010dc8ae6
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-08-09 02:55:45 -0700

    revise

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - program that print without printf, put
+ * return: 0 success
+ */
+int main (void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(1, message, sizeof(message));
+	putchar('\n');
+	return 0;
+}
