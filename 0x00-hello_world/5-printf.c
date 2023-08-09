1cd5aa9cf4ef7cf3e337d85a47d74b6690e439fd
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-08-09 01:21:52 -0700

    revise

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints on the screen
+ * return: 0 (success)
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
