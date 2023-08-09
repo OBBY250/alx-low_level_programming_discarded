fb37fd19c27bfcfcf4ee955530b015cb3b8e4578
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-08-09 00:11:47 -0700

    revise

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - program that prints _putchar using _putchar
+ * Return: 0   (success)
+ */
+
+int main(void)
+{
+	char a;
+	a = "_putchar";
+
+	_putchar("a\n");
+	return (0);
+}
