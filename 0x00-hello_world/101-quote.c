c62a1ca753b06da0f38e65c79f022257ea0c92c1
OBBYBOLIVZ <bizimunguolivier250@gmail.com>
2023-08-09 03:29:34 -0700

    revise

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 /**
  * main - program that print without printf, put
- * return: 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-	putchar('\n');
 	return (1);
 }
